a3814b768bdd96cda1681128f48d650d0052eb36
Kelly Kemnitz <kellygkemnitz@gmail.com>
2025-03-01 11:40:41 -0600

    Changing structure and working on adding track results to dashboard

--- app.py
+++ app.py
@@ -1,36 +1,61 @@
-from race_results import CrossCountry
-
 import dash
 from dash import dash_table
 from dash import dcc
 from dash import html
-
+from dash.dependencies import Input, Output
+from modules.race_results import CrossCountry, Track
 
 app = dash.Dash(__name__)
 app.title = "Race Times Dashboard"
 
-# Initialize the CrossCountry class
+# Initialize classes
 cross_country = CrossCountry()
+track = Track()
 
 app.layout = html.Div([
     html.H1("Race Times Dashboard", style={'textAlign': 'center'}),
     
-    # Data Table
-    dash_table.DataTable(
-        id='race-table',
-        columns=[{"name": i, "id": i} for i in cross_country._cross_country_df.columns],
-        data=cross_country._cross_country_df.to_dict('records'), 
-        style_table={'overflowX': 'auto'},
-        style_cell={'textAlign': 'left', 'fontFamily': 'Arial'},
-        style_header={'backgroundColor': 'lightgrey', 'fontWeight': 'bold'}
-    ),
+    # Tabs
+    dcc.Tabs(id='tabs', value='cross_country', children=[
+        dcc.Tab(label='Cross Country Results', value='cross_country'),
+        dcc.Tab(label='Track Results', value='track')
+    ]),
     
-    # Graph
-    dcc.Graph(
-        id='race-graph',
-        figure=cross_country._create_cross_country_plots
-    )
+    html.Div(id='tabs-content')
 ])
+
+@app.callback(Output('tabs-content', 'children'), Input('tabs', 'value'))
+def render_content(tab):
+    if tab == 'cross_country':
+        return html.Div([
+            dash_table.DataTable(
+                id='race-table',
+                columns=[{"name": i, "id": i} for i in cross_country._cross_country_df.columns],
+                data=cross_country._cross_country_df.to_dict('records'), 
+                style_table={'overflowX': 'auto'},
+                style_cell={'textAlign': 'left', 'fontFamily': 'Arial'},
+                style_header={'backgroundColor': 'lightgrey', 'fontWeight': 'bold'}
+            ),
+            dcc.Graph(
+                id='race-graph',
+                figure=cross_country._create_cross_country_plots()
+            )
+        ])
+    elif tab == 'track':
+        return html.Div([
+            dash_table.DataTable(
+                id='race-table',
+                columns=[{"name": i, "id": i} for i in track._track_df.columns],
+                data=track._track_df.to_dict('records'), 
+                style_table={'overflowX': 'auto'},
+                style_cell={'textAlign': 'left', 'fontFamily': 'Arial'},
+                style_header={'backgroundColor': 'lightgrey', 'fontWeight': 'bold'}
+            ),
+            dcc.Graph(
+                id='race-graph',
+                figure=track._create_track_plots()
+            )
+        ])
 
 if __name__ == "__main__":
     app.run_server(debug=True)
--- modules/race_results.py
+++ modules/race_results.py
@@ -1,4 +1,3 @@
-import json
 import plotly.graph_objects as go
 import pandas as pd
 
@@ -55,6 +54,66 @@ class CrossCountry:
                 tickmode='array',
                 tickvals=cross_country_results['Date'],
                 ticktext=cross_country_results['Date'],
+                tickangle=45,
+            ),
+            hovermode='closest'
+        )
+        
+        return fig
+    
+class Track:
+    def __init__(self):
+        self._track_data = 'data/track-results.json'
+        self._track_df = self._convert_to_df(self._track_data)
+        self._race_times = self._times_to_seconds(self._track_df)
+        self._create_track_plots = self._create_track_plots(self._race_times)
+
+    def _convert_to_df(self, file_path):
+        try:
+            df = pd.read_json(self._track_data)
+            return df
+        except FileNotFoundError as e:
+            print (f'Error {e}')
+            return None
+        except Exception as e:
+            print(f'An unexpected error occurred: {e}')
+            return None
+        
+    def _times_to_seconds(self, track_df):
+        track_df['Seconds'] = track_df['Individual Time'].apply(lambda x: int(x.split(':')[0]) * 60 + float(x.split(':')[1]))
+        return track_df
+
+    def _create_track_plots(self, track_results):
+        fig = go.Figure()
+
+        track_results = track_results.sort_values('Date')
+
+        hover_text = track_results.apply(lambda row:
+            f"Date: {row['Date']}<br>" +
+            f"Grade: {row['Grade']}<br>" +
+            f"Race: {row['Meet']}<br>" +
+            f"Time: {row['Individual Time']}", axis=1)
+
+        fig.add_trace(go.Scatter(
+            x=track_results['Date'],
+            y=track_results['Seconds'],
+            mode='lines+markers',
+            name='Race Times',
+            line=dict(color='blue'),
+            marker=dict(size=8, color='blue'),
+            text=hover_text,
+            hoverinfo='text'
+        ))
+        
+        fig.update_layout(
+            title='Race History',
+            xaxis_title='Date',
+            yaxis_title='Time (seconds)',
+            xaxis=dict(
+                type='category',
+                tickmode='array',
+                tickvals=track_results['Date'],
+                ticktext=track_results['Date'],
                 tickangle=45,
             ),
             hovermode='closest'
